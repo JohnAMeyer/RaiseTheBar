8a97636d67a3f2f965c0602186b9e9b644d2c1c6
Abby Greentree <agreentr@nd.edu>
2016-09-30 23:56:06 -0400

    Started working on slave side

--- app/src/main/java/edu/nd/raisethebar/SlaveActivity.java
+++ app/src/main/java/edu/nd/raisethebar/SlaveActivity.java
@@ -2,15 +2,33 @@ package edu.nd.raisethebar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
 /**
  * This class is for the device that is recording the data
  */
+
+
 public class SlaveActivity extends AppCompatActivity {
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+
+    public SlaveActivity(){
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slave);
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
+     protected void onPause(){
+       
+     }
+
 }
