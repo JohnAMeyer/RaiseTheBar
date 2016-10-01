a14aa81dec2880d7a82de85d61c8309c13df55f4
Abby Greentree <agreentr@nd.edu>
2016-10-01 19:22:00 -0400

    Updated functions.

--- app/src/main/java/edu/nd/raisethebar/MainActivity.java
+++ app/src/main/java/edu/nd/raisethebar/MainActivity.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -35,7 +36,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private boolean toggle = false;
     EditText weightEdit;
     private int weight=0;
-
+    double maxVel = 0D;
+    double[] velo;
+    double[] times;
+    int reps =0;
+    double norm_Of_g=0;
+    boolean goodform = true;
+    double[] smooth_accelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,20 +59,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
     protected void changed(){
+        process();
         GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
 
-        });
+        int lengthAccelerometer = accelerometer_event.size();
+
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>();
+
+        for (int i = 0; i < lengthAccelerometer; i++) {
+            //series.appendData(new DataPoint((times[i]), (velo[i])), true, lengthAccelerometer);
+            series.appendData(new DataPoint((times[i]), (smooth_accelerometer[i])), true, lengthAccelerometer);
+        }
+
         graph.addSeries(series);
-        graph.setTitle("Acceleration of the Bar");
+        graph.setTitle("Velocity of the Bar");
+        graph.getViewport().setScrollable(true);
 
         TextView form = (TextView) findViewById(R.id.textForm);
-        boolean goodform = false;
         if (goodform) {
             form.setText("Good Form");
             form.setTextColor(0xff00ff00);
@@ -74,15 +84,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             form.setTextColor(0xffff0000);
         }
 
-        int countReps = 5;
-        String numberRepsAsString = Integer.toString(countReps);
+        int countReps = reps/2;
+        String numberRepsAsString = countReps+"";
         TextView numberReps = (TextView) findViewById(R.id.textNumberReps);
         numberReps.setText(numberRepsAsString);
 
-        float countMaxSpeed = 10;
-        String numberMaxSpeedAsString = Float.toString(countMaxSpeed);
+        double countMaxSpeed = ((long)(maxVel*100))/100D;
+        String numberMaxSpeedAsString = Double.toString(countMaxSpeed);
         TextView maxSpeed = (TextView) findViewById(R.id.textMaxSpeed);
         maxSpeed.setText(numberMaxSpeedAsString);
+
+        String weightAsString=Integer.toString(weight);
+        TextView weightview = (TextView) findViewById(R.id.textWeight);
+        weightview.setText(weightAsString);
     }
     protected void process(){
         long time_0 = accelerometer_event.get(0).time;
@@ -91,18 +105,52 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             //tu.time /= 1000000000;
         }
         LinkedList<Tuple> reimannVel = new LinkedList<>();
+        velo = new double[accelerometer_event.size()];
+        times = new double[accelerometer_event.size()];
         reimannVel.add(new Tuple(ZERO,0));
-        double maxVel = 0D;
         for (int i = 1; i < accelerometer_event.size();i++){
             double[] vel = new double[3];
             long time = (accelerometer_event.get(i).time-accelerometer_event.get(i-1).time);
-            vel[0] = (accelerometer_event.get(i).data[0]*time + reimannVel.peekLast().data[0]);
-            vel[1] = (accelerometer_event.get(i).data[1]*time + reimannVel.peekLast().data[1]);
-            vel[2] = (accelerometer_event.get(i).data[2]*time + reimannVel.peekLast().data[2]);
+            vel[0] = (accelerometer_event.get(i).data[0]*time + reimannVel.peekLast().data[0])/1000000000D;
+            vel[1] = (accelerometer_event.get(i).data[1]*time + reimannVel.peekLast().data[1])/1000000000D;
+            vel[2] = (accelerometer_event.get(i).data[2]*time + reimannVel.peekLast().data[2])/1000000000D;
             reimannVel.add(new Tuple(vel,accelerometer_event.get(i).time));
-            double velNorm = Math.hypot(Math.hypot(vel[0],vel[1]),vel[2]);
-            maxVel = (velNorm>maxVel)?velNorm : maxVel;
+            Log.d("VEL",reimannVel.peekLast().toString());
+            velo[i] = Math.hypot(Math.hypot(vel[0],vel[1]),vel[2]);
+            times[i] = accelerometer_event.get(i).time/1000000000D;
+            maxVel = (velo[i]>maxVel)?velo[i] : maxVel;
         }
+
+        smooth_accelerometer = new double[accelerometer_event.size()];
+
+        for (int i = 2; i<(accelerometer_event.size()-2);i++){
+            smooth_accelerometer[i]=((accelerometer_event.get(i-2).data[2]+accelerometer_event.get(i-1).data[2]
+            +accelerometer_event.get(i).data[2]+accelerometer_event.get(i+1).data[2]+accelerometer_event.get(i+2).data[2])/5);
+        }
+        for (int i = 0; i<(accelerometer_event.size()-1);i++){
+            if ((smooth_accelerometer[i]*smooth_accelerometer[i+1])<0){
+               reps++;
+           }
+        }
+
+        norm_Of_g = Math.sqrt(tilt_event.get(1).data[0] * tilt_event.get(1).data[0]+tilt_event.get(1).data[1]*tilt_event.get(1).data[1]
+                + tilt_event.get(1).data[2]*tilt_event.get(1).data[2]);
+
+
+        for (int i = 0; i<(tilt_event.size());i++){
+            tilt_event.get(i).data[0]/=norm_Of_g;
+            tilt_event.get(i).data[1]/=norm_Of_g;
+            tilt_event.get(i).data[2]/=norm_Of_g;
+        }
+
+        double [] rotation_array = new double [tilt_event.size()];
+        for (int i = 0; i<(tilt_event.size());i++){
+            rotation_array[i]=Math.round(Math.toDegrees(Math.atan2(tilt_event.get(i).data[0], tilt_event.get(i).data[2])));
+            if (rotation_array[i]>=20){
+                goodform=false;
+            }
+        }
+
 
 
     }
@@ -118,12 +166,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             setContentView(R.layout.activity_master);
             changed();
         } else {
-            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-            mSensorManager.registerListener(this, mOrientation, SensorManager.SENSOR_DELAY_NORMAL);
+            TextView onbutton = (TextView) findViewById(R.id.StartClick);
+            onbutton.setText("Stop Recording");
+            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+            mSensorManager.registerListener(this, mOrientation, SensorManager.SENSOR_DELAY_UI);
             toggle = true;
             accelerometer_event=new ArrayList<Tuple>();
             tilt_event=new ArrayList<Tuple>();
-            weight = Integer.parseInt(weightEdit.getText().toString());
+            try{
+                weight = Integer.parseInt(weightEdit.getText().toString());
+            }catch(Exception e){
+                weight=0;
+            }
 
         }
     }
@@ -162,6 +216,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         public Tuple(double[] data, long time) {
             this.data = data;
+            this.time = time;
+        }
+        public Tuple(float[] data, long time) {
+            this.data = new double[data.length];
+            for(int i = 0; i < data.length; i++){
+                this.data[i] = data[i];
+            }
             this.time = time;
         }
         @Override
