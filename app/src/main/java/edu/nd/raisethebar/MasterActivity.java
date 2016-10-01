a34f69405d40115bb7d601dcebcb9e7f6a5f829f
Aemile Donoghue <aemiledonoghue@gmail.com>
2016-09-30 23:54:16 -0400

    Started working on user side

--- app/src/main/java/edu/nd/raisethebar/MasterActivity.java
+++ app/src/main/java/edu/nd/raisethebar/MasterActivity.java
@@ -3,6 +3,11 @@ package edu.nd.raisethebar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+
 /**
  * This class is for the displaying side of the app
  */
@@ -12,5 +17,19 @@ public class MasterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master);
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+
+        });
+        graph.addSeries(series);
     }
 }
