513530d3631470c510605bfdd9accb49c7eb5c99
Jack Meyer <jack10042@gmail.com>
2016-09-30 22:18:32 -0400

    Added all three activities

--- app/src/main/java/edu/nd/raisethebar/MainActivity.java
+++ app/src/main/java/edu/nd/raisethebar/MainActivity.java
@@ -4,13 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 /**
- * This class is for the displaying side of the app
+ * This class combines both functionalities
  */
-public class MasterActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_master);
+        setContentView(R.layout.activity_main);
     }
 }
