0ff56c24f87ff372f63f7c7b4ff9923486726259
Jack Meyer <jack10042@gmail.com>
2016-10-19 00:56:27 -0400

    Messed around with the splash screen stuff more - need to downsize image.

--- app/src/main/java/edu/nd/raisethebar/SelectorActivity.java
+++ app/src/main/java/edu/nd/raisethebar/SelectorActivity.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Toast;
+
+import static android.R.attr.tag;
 
 /**
  * Created by aemiledonoghue on 10/6/16.
@@ -16,22 +19,21 @@ import android.widget.Toast;
 
 
 public class SelectorActivity extends AppCompatActivity {
+    private static final String TAG = "RTB-SelectorActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_selector);
-
-        Button button_new_session = (Button) findViewById(R.id.button_new_session);
     }
 
-    public void new_session(View view) {
+    public void newSession(View view) {
         Intent intent = new Intent(this, GymSelectorActivity.class);
         startActivity(intent);
     }
 
-    public void previous_session(View view) {
+    public void previousSession(View view) {
         Intent intent = new Intent(this, CalendarActivity.class);
         startActivity(intent);
     }
