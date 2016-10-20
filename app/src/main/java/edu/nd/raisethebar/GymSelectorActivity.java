031f23b79aeed19e6352bbd71adff8394301d21f
Jack Meyer <jack10042@gmail.com>
2016-10-19 20:35:50 -0400

    Added sample data for machines and actual functioning program flow.

--- app/src/main/java/edu/nd/raisethebar/GymSelectorActivity.java
+++ app/src/main/java/edu/nd/raisethebar/GymSelectorActivity.java
@@ -1,35 +1,66 @@
 package edu.nd.raisethebar;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import static android.R.attr.key;
+import static android.R.attr.value;
 
 /**
  * Created by jack1 on 10/18/2016.
  */
 
 public class GymSelectorActivity extends AppCompatActivity {
-    private static final String TAG = "RTB-GymSelectorActivity";
+    private static final String TAG = "RTB-GymSelector";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gym_selector);
         //try and obtain gps infor, but if locations is refused or turned off, have location prompt
         //https://developer.android.com/guide/topics/location/strategies.html
-        JSONObject result;
+        JSONArray result = null;
+        String[] items = null;
         try {
-             result = RawToJSON.toJSON(this, R.raw.gyms);
+            result = RawToJSON.toJSON(this, R.raw.gyms).getJSONArray("gyms");
+            items = new String [result.length()];
+            for(int i = 0; i<result.length();i++){
+                items[i] = result.getJSONObject(i).get("name").toString();
+            }
         } catch (Exception e){
             Log.e(TAG,"JSON Error",e);
         }
-        //String[] items = result...;
         ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
         //populate list with items from gyms.php - may need to use a more advanced array adaptor if simple text does not work
+        ListView vid = (ListView) this.findViewById(R.id.gym_list);
+        vid.setAdapter(itemsAdapter);
 
         //on item select, fire new activity
+        final Context ct = this;
+        final JSONArray arr = result;
+        vid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(ct, MachineSelectorActivity.class);
+                //If you wanna send any data to nextActicity.class you can use
+                try {
+                    Log.d(TAG,"Clicked " + arr.getJSONObject(position).toString());
+                    i.putExtra("JSON", arr.getJSONObject(position).toString());
+                }catch (Exception e){
+                    Log.e(TAG,"OnClickHandler",e);
+                }
+
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/edu/nd/raisethebar/MachineSelectorActivity.java
+++ app/src/main/java/edu/nd/raisethebar/MachineSelectorActivity.java
@@ -1,16 +1,75 @@
 package edu.nd.raisethebar;
 
+import android.content.Context;
+import android.content.Intent;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MachineSelectorActivity extends AppCompatActivity {
-
+    private static final String TAG = "RTB-MachineSelector";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_machine_selector);
+        Intent intent = getIntent();
+        int gymId = -1;
+        try {
+            JSONObject gym = new JSONObject(intent.getStringExtra("JSON"));
+            gymId = gym.getInt("id");
+            //makes call to server with id
+
+        } catch(JSONException e){
+            Log.e(TAG,"JSON Error",e);
+        }
+        Log.d(TAG,gymId+"");
+
+        JSONArray result = null;
+        String[] items = null;
+        try {
+            result = RawToJSON.toJSON(this, R.raw.machines).getJSONArray("machines");
+            items = new String [result.length()];
+            for(int i = 0; i<result.length();i++){
+                items[i] = result.getJSONObject(i).get("name").toString();
+            }
+        } catch (Exception e){
+            Log.e(TAG,"JSON Error",e);
+        }
         //use adaptor to populate listview with results from devices.php
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        //populate list with items from gyms.php - may need to use a more advanced array adaptor if simple text does not work
+        ListView vid = (ListView) this.findViewById(R.id.machine_list);
+        vid.setAdapter(itemsAdapter);
+
+        //on item select, fire new activity
+        final Context ct = this;
+        final JSONArray arr = result;
+        vid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent i = new Intent(ct, RecordActivity.class);
+                try {
+                    i.putExtra("JSON", arr.getJSONObject(position).toString());
+                }catch (Exception e){
+                    Log.e(TAG,"OnClickHandler",e);
+                }
+
+                startActivity(i);
+            }
+        });
     }
+
+
 
     //onclick attempt to connect with device
 }
